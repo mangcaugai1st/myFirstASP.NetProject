5323a31ed3e2fb50fdaa7cb637d99baa366419e7
mangcaugai 1st <mangcaugai1st@gmail.com>
2024-01-16 17:58:40 +0700

    16-Jan-2024

--- Controllers/AController.cs
+++ Controllers/AController.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ASP.netMVC.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ASP.netMVC.Controllers
 {
     public class AController : Controller
     {
+        
         private readonly ILogger<HomeController> _logger;
 
         public AController(ILogger<HomeController> logger)
@@ -12,33 +14,39 @@ namespace ASP.netMVC.Controllers
         }
         public IActionResult Index()
         {
-            return View("Index");
+            List<Post> data = new List<Post>
+            {
+                new Post() { Thumbnail = "ThinkPad-logo.webp" , Title = "Review ThinkPad E590 sau 4 năm sử dụng", Link = "ThinkPad_E590"},
+                new Post() { Thumbnail = "Melaleuca-leucadendra.webp" , Title = "Đôi điều về API Melafix", Link = "API_Melafix"}
+            };
+            return View(data);
         }
+        
         public IActionResult ThinkPad_E590()
         {
-            return View("ThinkPad_E590");
-        }
-        [HttpPost]
-        public IActionResult Index(double nDegrees, string temperatureUnit)
-        {
-            double a = nDegrees;
-            string b = temperatureUnit;
-            double result;
-            string resultWithUnitSymbol;
-            switch (b)
+            List<string>? img = new List<string>
             {
-                case "Fahrenheit":
-                    result = (5 * a - 160) / 9;
-                    resultWithUnitSymbol = string.Concat(Math.Round(result, 2), "°C");
-                    ViewBag.ResultWithUnitSymbol = resultWithUnitSymbol;
-                    break;
-                case "Celsius":
-                    result = (9 * a / 5) + 32;
-                    resultWithUnitSymbol = string.Concat(Math.Round(result, 2), "°F");
-                    ViewBag.ResultWithUnitSymbol = resultWithUnitSymbol;
-                    break;
-            }
-            return View("Index");
+                "Ubuntu-Logo.png", //  0
+                "Fedora_Logo.png", // 1 
+                "Red-Hat-logo.png", // 2
+                "OpenSUSE_Logo.png", // 3
+                "btw_i_use_arch.jpg", // 4
+                "gentoo-linux-logo.png", //5
+                "20230729_162524.png", //6
+                "LenovoVantage.png", //7
+                "", //8
+                "", //9
+            };
+            return View(img);
+        }
+        public IActionResult API_Melafix()
+        {
+            List<string>? img = new List<string>
+            {
+                "Melaleuca_leucadendra_-_Köhler–s_Medizinal-Pflanzen-092.jpg",
+                "Camellia.jpg" 
+            };
+            return View(img);
         }
     }
 }
---
+++ Controllers/CController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.netMVC.Controllers
+{
+    public class CController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(double nDegrees, string temperatureUnit)
+        {
+            double a = nDegrees;
+            string b = temperatureUnit;
+            double result;
+            string resultWithUnitSymbol;
+            switch (b)
+            {
+                case "Fahrenheit":
+                    result = (5 * a - 160) / 9;
+                    resultWithUnitSymbol = string.Concat(Math.Round(result, 2), "°C");
+                    ViewBag.ResultWithUnitSymbol = resultWithUnitSymbol;
+                    break;
+                case "Celsius":
+                    result = (9 * a / 5) + 32;
+                    resultWithUnitSymbol = string.Concat(Math.Round(result, 2), "°F");
+                    ViewBag.ResultWithUnitSymbol = resultWithUnitSymbol;
+                    break;
+            }
+            return View("Index");
+        }
+    }
+}
